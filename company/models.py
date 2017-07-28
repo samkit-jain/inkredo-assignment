11d6713716bca275d2bee4c990d9fc1e9b6cc68a
Samkit Jain <samkitjain5795@gmail.com>
2017-07-28 12:05:13 +0530

    models created

--- company/models.py
+++ company/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+'''
+sqlmigrate company 0001
+BEGIN;
+--
+-- Create model Company
+--
+CREATE TABLE "company_company" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL);
+COMMIT;
+
+'''
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from company.models import Company
 
-# Create your models here.
+class Users(models.Model):
+	name = models.CharField(max_length=50)
+	job_title = models.CharField(max_length=50)
+	age = models.IntegerField()
+	company_val = models.ForeignKey(Company)
+	
+	GENDER_CHOICES = (
+		('M', 'Male'),
+		('F', 'Female'),
+	)
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+	def __str__(self):
+		return self.name
+
+'''
+sqlmigrate users 0001
+BEGIN;
+--
+-- Create model Users
+--
+CREATE TABLE "users_users" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(50) NOT NULL, "job_title" varchar(50) NOT NULL, "age" integer NOT NULL, "gender" varchar(1) NOT NULL, "company_val_id" integer NOT NULL);
+ALTER TABLE "users_users" ADD CONSTRAINT "users_users_company_val_id_eceaddf5_fk_company_company_id" FOREIGN KEY ("company_val_id") REFERENCES "company_company" ("id") DEFERRABLE INITIALLY DEFERRED;
+CREATE INDEX "users_users_company_val_id_eceaddf5" ON "users_users" ("company_val_id");
+COMMIT;
+'''
